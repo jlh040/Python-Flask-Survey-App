ea46dad0ab6167c667bc8d5496807e3dde433f69
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-03 23:12:11 -0700

    added root route, added base and home html templates

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, redirect, request, flash, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '0c2b6x'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route('/')
+def show_start_page():
+    """Display the title and instructions of the survey,
+    and a button to start the survey.
+    """
+
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('home.html', title = title, instructions = instructions)
